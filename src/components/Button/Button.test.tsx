d2d4794e35ae2da3ba3bcdc5722a222f19669fe7
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-07 22:14:19 +0100

    Add test for Button component

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+describe("Given a Button component", () => {
+  describe("When it is rendred", () => {
+    test("Then it should show a button with the text 'Log in'", () => {
+      const buttonText = "Log in";
+
+      render(<Button text={buttonText} />);
+
+      const expectedButton = screen.getByRole("button", { name: buttonText });
+
+      expect(expectedButton).toBeInTheDocument();
+    });
+  });
+});
