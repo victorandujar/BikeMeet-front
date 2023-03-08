88459c7fbed76e539bdc2e9e485a1c086df6336d
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-08 17:38:10 +0100

    Create custom store for testing

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,25 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  PreloadedState,
+  combineReducers,
+} from "@reduxjs/toolkit";
 import { userReducer } from "./features/usersSlice/usersSlice";
 
-export const store = configureStore({
-  reducer: { user: userReducer },
-});
+const rootReducer = combineReducers({ user: userReducer });
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
+
+export const store = setupStore();
 
 export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
