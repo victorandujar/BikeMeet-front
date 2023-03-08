e75e1cbff72f95343749c4d1910cedd73710b6c2
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-08 17:37:10 +0100

    Add test for useUser custom hook

---
+++ src/hooks/useUser/useUser.test.tsx
@@ -0,0 +1,54 @@
+import { renderHook } from "@testing-library/react";
+import Wrapper from "../../mocks/Wrapper";
+import { store } from "../../store/store";
+import { CustomTokenPayload, UserCredentials } from "./types";
+import useUser from "./useUser";
+import decodeToken from "jwt-decode";
+import { User } from "../../types/types";
+import { loginUserActionCreator } from "../../store/features/usersSlice/usersSlice";
+
+jest.mock("jwt-decode", () => jest.fn());
+
+const spy = jest.spyOn(store, "dispatch");
+
+beforeAll(() => {
+  jest.clearAllMocks();
+});
+
+const userCredentials: UserCredentials = {
+  email: "victor@andujar.org",
+  password: "dkldjdklsdj",
+};
+
+const mockTokenPayload: CustomTokenPayload = {
+  email: "victor@andujar.org",
+  id: "12345678",
+};
+
+const mockToken = "vik27634fvj";
+
+describe("Given a useUser custom hook", () => {
+  describe("When the loginUser function is called", () => {
+    test("Then it should call the dispatch", async () => {
+      const {
+        result: {
+          current: { loginUser },
+        },
+      } = renderHook(() => useUser(), { wrapper: Wrapper });
+
+      (decodeToken as jest.MockedFunction<typeof decodeToken>).mockReturnValue(
+        mockTokenPayload
+      );
+
+      const mockLoginUser: User = {
+        email: mockTokenPayload.email,
+        id: mockTokenPayload.id,
+        token: mockToken,
+      };
+
+      await loginUser(userCredentials);
+
+      expect(spy).toHaveBeenCalledWith(loginUserActionCreator(mockLoginUser));
+    });
+  });
+});
