234a345d47c775b43552016f362f3b89af68aeab
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-09 18:45:05 +0100

    Create theme provider for styles

---
+++ src/styles/Theme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  fonts: {
+    primary: "Inter",
+    secondary: "Montserrat",
+  },
+  colors: {
+    mainColorButton: "#0D23AF",
+    mainColorText: "#fff",
+    secondaryColorText: "#000",
+    thirdColorText: "#0D23AF",
+  },
+};
+
+export default theme;
