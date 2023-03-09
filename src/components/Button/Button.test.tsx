3974638b440fbb0cef8d958de9ca2c8c47dbf9cb
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-09 15:50:27 +0100

    Add Layout to App and add GlobalStyles to Button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import GlobalStyles from "../../styles/GlobalStyles";
 import Button from "./Button";
 
 describe("Given a Button component", () => {
@@ -6,7 +7,12 @@ describe("Given a Button component", () => {
     test("Then it should show a button with the text 'Log in'", () => {
       const buttonText = "Log in";
 
-      render(<Button text={buttonText.toUpperCase()} />);
+      render(
+        <>
+          <GlobalStyles />
+          <Button text={buttonText.toUpperCase()} />
+        </>
+      );
 
       const expectedButton = screen.getByRole("button", {
         name: buttonText.toUpperCase(),
