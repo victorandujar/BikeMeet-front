17c9fb0d5b151a4f83c4d2dd3fa8f48587f7dd28
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-10 20:11:27 +0100

    Change log out icon while its not logged for register icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,10 +4,14 @@ import {
   faRectangleList,
   faPlus,
   faRightFromBracket,
+  faUserPlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useAppSelector } from "../../store/hooks";
 
 const Header = (): JSX.Element => {
+  const { isLogged } = useAppSelector((state) => state.user);
+
   return (
     <HeaderStyled className="main-header">
       <div className="main-header__logo logo">
@@ -20,14 +24,19 @@ const Header = (): JSX.Element => {
         />
         <h2 className="logo__title">BIKEMEET</h2>
       </div>
+
       <nav className="main-header__navigation">
         <FontAwesomeIcon icon={faHouseChimney} className="fa-solid--active" />
         <FontAwesomeIcon icon={faRectangleList} className="fa-solid--active" />
         <FontAwesomeIcon icon={faPlus} className="fa-solid--active" />
-        <FontAwesomeIcon
-          icon={faRightFromBracket}
-          className="fa-solid--active"
-        />
+        {isLogged ? (
+          <FontAwesomeIcon
+            icon={faRightFromBracket}
+            className="fa-solid--active"
+          />
+        ) : (
+          <FontAwesomeIcon icon={faUserPlus} />
+        )}
       </nav>
     </HeaderStyled>
   );
