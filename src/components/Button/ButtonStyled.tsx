990a02008ea94e6eca63316e2d639de33d3c9f67
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-10 19:36:54 +0100

    Fix paddings for 320px width design

--- src/components/Button/ButtonStyled.tsx
+++ src/components/Button/ButtonStyled.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const ButtonStyled = styled.button`
   background-color: ${(props) => props.theme.colors.mainColorButton};
   height: 3.5rem;
-  width: 18.75rem;
+  width: 18.125rem;
   color: ${(props) => props.theme.colors.mainColorText};
   font-weight: 800;
   text-transform: uppercase;
