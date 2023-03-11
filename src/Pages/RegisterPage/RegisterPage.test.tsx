b97bc47bb4bac668e9fed91c84309c136ae55be9
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-11 12:01:11 +0100

    Add testing for RegisterPage component

---
+++ src/Pages/RegisterPage/RegisterPage.test.tsx
@@ -0,0 +1,29 @@
+import { screen } from "@testing-library/react";
+import { renderRouterWithProviders } from "../../testUtils/renderWithProviders";
+import RegisterPage from "./RegisterPage";
+
+describe("Given a RegisterPage component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a header with the text 'Sign up'", () => {
+      const headertext = "Sign up";
+
+      renderRouterWithProviders(<RegisterPage />);
+
+      const expectedHeader = screen.getByRole("heading", { name: headertext });
+
+      expect(expectedHeader).toBeInTheDocument();
+    });
+
+    test("Then it should show a button with the text 'SIGN UP'", () => {
+      const buttonText = "Sign up";
+
+      renderRouterWithProviders(<RegisterPage />);
+
+      const expectedButton = screen.getByRole("button", {
+        name: buttonText,
+      });
+
+      expect(expectedButton).toBeInTheDocument();
+    });
+  });
+});
