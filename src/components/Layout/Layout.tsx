5f30eeed3c793e01f53028ff2a447d59f4489dfe
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-13 20:25:01 +0100

    Fix Layout test because of adding actions to uiSlice

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from "react-router";
-import { ToastContainer } from "react-toastify";
 import { useAppSelector } from "../../store/hooks";
 import Header from "../Header/Header";
 import Loader from "../Loader/Loader";
+import Modal from "../Modal/Modal";
 
 const Layout = (): JSX.Element => {
   const { isLoading } = useAppSelector((state) => state.ui);
@@ -10,7 +10,7 @@ const Layout = (): JSX.Element => {
   return (
     <>
       <Header />
-      <ToastContainer hideProgressBar />
+      <Modal />
       <main>
         {isLoading && <Loader />}
         <Outlet />
