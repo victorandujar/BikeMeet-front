51f3a111131a07c24b5dce64bd23ba4ff6dee830
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-14 20:43:33 +0100

    Add to router the PrivatePage

--- src/routers/types.ts
+++ src/routers/types.ts
@@ -1,11 +1,13 @@
 interface Routes {
   login: string;
   register: string;
+  mySpace: string;
 }
 
 const endpoints: Routes = {
   login: "/login",
   register: "/sign-up",
+  mySpace: "/my-space",
 };
 
 export default endpoints;
