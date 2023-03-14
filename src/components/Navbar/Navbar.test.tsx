e94231a64e37ffc87d14d4252f1a6b2dcddc36e0
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-14 21:44:24 +0100

    Add testing for Navbar component

---
+++ src/components/Navbar/Navbar.test.tsx
@@ -0,0 +1,17 @@
+import { screen } from "@testing-library/react";
+import { renderRouterWithProviders } from "../../testUtils/renderWithProviders";
+import { UserState } from "../../types/users/types";
+import Navbar from "./Navbar";
+
+describe("Given a Navbar component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a font awsome icon from Home", () => {
+      const user: UserState = { email: "", id: "", isLogged: true, token: "" };
+      renderRouterWithProviders(<Navbar />, { user: user });
+
+      const expectedNavBar = screen.getByRole("navigation");
+
+      expect(expectedNavBar).toBeInTheDocument();
+    });
+  });
+});
