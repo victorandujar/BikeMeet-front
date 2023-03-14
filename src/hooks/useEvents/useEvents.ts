341c7424fed6bdad4b0aff83a811599e94df1bf5
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-14 18:42:19 +0100

    Create getUserEvents function at useEvents hook

--- src/hooks/useEvents/useEvents.ts
+++ src/hooks/useEvents/useEvents.ts
@@ -1,18 +1,21 @@
 import { useCallback } from "react";
 import { loadEventsActionCreator } from "../../store/features/eventsSlice/eventsSlice";
 import {
+  openModalActionCreator,
   setIsLoadingActionCreator,
   unsetIsLoadingActionCreator,
 } from "../../store/features/uiSlice/uiSlice";
-import { useAppDispatch } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { EventsData } from "../../types/events/types";
 
 const apiUrl = process.env.REACT_APP_URL_API;
 const pathEvents = "/events";
 const getEventsEndpoint = "/";
+const getUserEventsEndpoint = "/my-events";
 
 const useEvents = () => {
   const dispatch = useAppDispatch();
+  const { token } = useAppSelector((state) => state.user);
 
   const getEvents = useCallback(async () => {
     try {
@@ -31,11 +34,47 @@ const useEvents = () => {
       dispatch(loadEventsActionCreator(events));
     } catch (error) {
       dispatch(unsetIsLoadingActionCreator());
+      dispatch(
+        openModalActionCreator({
+          isError: true,
+          isSuccess: false,
+          message: "We couldn't retrieve events. Try again!",
+        })
+      );
       return (error as Error).message;
     }
   }, [dispatch]);
 
-  return { getEvents };
+  const getUserEvents = useCallback(async () => {
+    try {
+      dispatch(setIsLoadingActionCreator());
+
+      const response = await fetch(
+        `${apiUrl}${pathEvents}${getUserEventsEndpoint}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const { events } = (await response.json()) as EventsData;
+
+      dispatch(unsetIsLoadingActionCreator());
+      dispatch(loadEventsActionCreator(events));
+    } catch (error) {
+      dispatch(unsetIsLoadingActionCreator());
+      dispatch(
+        openModalActionCreator({
+          isError: true,
+          isSuccess: false,
+          message: "We couldn't retrieve events. Try again!",
+        })
+      );
+      return (error as Error).message;
+    }
+  }, [dispatch, token]);
+
+  return { getEvents, getUserEvents };
 };
 
 export default useEvents;
