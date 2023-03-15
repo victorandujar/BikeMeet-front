aeec4e2a6ae13fc5380fa746bb5a6bdb9f7101cd
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-15 15:45:37 +0100

    Add testing for conditional rendering for my space buttons

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,12 +1,13 @@
 import { screen } from "@testing-library/react";
 import { mockEventSaCosta } from "../../mocks/mocks";
-import { renderWithProviders } from "../../testUtils/renderWithProviders";
+import { renderRouterWithProviders } from "../../testUtils/renderWithProviders";
 import Card from "./Card";
+import "react-router-dom";
 
 describe("Given a Card component", () => {
   describe("When it is rendered", () => {
     test("Then it should show an image of the event", () => {
-      renderWithProviders(<Card event={mockEventSaCosta} />);
+      renderRouterWithProviders(<Card event={mockEventSaCosta} />);
 
       const expectedImage = screen.getByRole("img");
 
@@ -16,13 +17,25 @@ describe("Given a Card component", () => {
     test("Then it should show a heading with the text 'Sa costa'", () => {
       const headingText = "Sa costa";
 
-      renderWithProviders(<Card event={mockEventSaCosta} />);
+      renderRouterWithProviders(<Card event={mockEventSaCosta} />);
 
       const expectedHeading = screen.getByRole("heading", {
         name: headingText,
       });
 
       expect(expectedHeading).toBeInTheDocument();
+    });
+
+    test("Then it should show a button with an aria-label 'delete'", () => {
+      const text = "delete";
+
+      renderRouterWithProviders(<Card event={mockEventSaCosta} />, {
+        user: { id: "2392382308901", email: "", isLogged: true, token: "" },
+      });
+
+      const expecteButton = screen.getByRole("button", { name: text });
+
+      expect(expecteButton).toBeInTheDocument();
     });
   });
 });
