e48c5e5090fd7fe20eb73d0c029694a634f952a2
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-16 09:57:41 +0100

    Add testing for RemoveButton component

---
+++ src/components/RemoveButton/RemoveButton.test.tsx
@@ -0,0 +1,35 @@
+import { act, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { mockEventMussara } from "../../mocks/mocks";
+import { renderWithProviders } from "../../testUtils/renderWithProviders";
+import RemoveButton from "./RemoveButton";
+
+const mockDeleteEvent = jest.fn();
+
+jest.mock("../../hooks/useEvents/useEvents", () => () => ({
+  deleteEvent: mockDeleteEvent,
+}));
+
+describe("Given a RemoveButton component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a button with an aria-label 'delete'", () => {
+      const text = "delete";
+
+      renderWithProviders(<RemoveButton event={mockEventMussara} />);
+
+      const expectedButton = screen.getByRole("button", { name: text });
+
+      expect(expectedButton).toBeInTheDocument();
+    });
+
+    test("Then it should call the function deleteEvent when the user click the button", async () => {
+      renderWithProviders(<RemoveButton event={mockEventMussara} />);
+
+      const submitButton = screen.getByRole("button");
+
+      await act(async () => await userEvent.click(submitButton));
+
+      expect(mockDeleteEvent).toHaveBeenCalledWith(mockEventMussara);
+    });
+  });
+});
