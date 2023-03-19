a46dd7f6d0d423d2ee95a8e05f666699fc5c34e0
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-19 12:02:19 +0100

    Add functionality to CreateForm component

--- src/components/CreateForm/CreateForm.tsx
+++ src/components/CreateForm/CreateForm.tsx
@@ -1,9 +1,75 @@
+import { useState } from "react";
+import useEvents from "../../hooks/useEvents/useEvents";
+import { EventCreateStructure } from "../../types/events/types";
 import ButtonForm from "../ButtonForm/ButtonForm";
 import CreateFormStyled from "./CreateFormStyled";
+import { optionsSelect } from "./optionsSelect";
 
 const CreateForm = (): JSX.Element => {
+  const { createEvent } = useEvents();
+
+  const [name, setName] = useState("");
+  const [distance, setDistance] = useState("");
+  const [type, setType] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState<File | string>("");
+
+  const handleName = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setName(value);
+  };
+
+  const handleDistance = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setDistance(value);
+  };
+
+  const handleType = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLSelectElement>) => {
+    setType(value);
+  };
+
+  const handleDate = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(value);
+  };
+
+  const handleDescription = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(value);
+  };
+
+  const handleImage = ({
+    target: { files },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    if (files !== null) {
+      setImage(files[0]);
+    }
+  };
+
+  const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newEvent: EventCreateStructure = {
+      date,
+      description,
+      distance,
+      image,
+      name,
+      type,
+    };
+
+    await createEvent(newEvent);
+  };
+
   return (
-    <CreateFormStyled className="create-form">
+    <CreateFormStyled className="create-form" onSubmit={onSubmitHandler}>
       <div className="create-form__field field">
         <label htmlFor="name" aria-label="name" className="field__name">
           Event name
@@ -13,6 +79,7 @@ const CreateForm = (): JSX.Element => {
           id="name"
           placeholder="Introduce an event name"
           className="field__data"
+          onChange={handleName}
         />
       </div>
       <div className="create-form__col2 col2">
@@ -29,6 +96,7 @@ const CreateForm = (): JSX.Element => {
             id="distance"
             placeholder="Km"
             className="col2__data"
+            onChange={handleDistance}
           />
         </div>
         <div className="create-form__field field">
@@ -39,7 +107,14 @@ const CreateForm = (): JSX.Element => {
             id="surface"
             placeholder="Surface"
             className="col2__data"
-          ></select>
+            onChange={handleType}
+          >
+            {optionsSelect.map((option) => (
+              <option value={option.type} key={option.key}>
+                {option.name}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
       <div className="create-form__field field">
@@ -51,6 +126,7 @@ const CreateForm = (): JSX.Element => {
           placeholder="Enter a valid date"
           id="date"
           className="field__data date"
+          onChange={handleDate}
         />
       </div>
       <div className="create-form__field field">
@@ -65,6 +141,7 @@ const CreateForm = (): JSX.Element => {
           id="description"
           placeholder="Add a description"
           className="field__data description"
+          onChange={handleDescription}
         />
       </div>
       <div className="create-form__field field">
@@ -76,6 +153,8 @@ const CreateForm = (): JSX.Element => {
           aria-label="image"
           id="image"
           className="field__data file"
+          onChange={handleImage}
+          placeholder="Add an image"
         />
       </div>
       <ButtonForm isDisabled={false} text={"create event"} />
