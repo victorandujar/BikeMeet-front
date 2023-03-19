57eedc3a8b8cc26bafa82c153275b170501b31c9
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-19 12:03:15 +0100

    Add testing for CreateForm functionality

--- src/components/CreateForm/CreateForm.test.tsx
+++ src/components/CreateForm/CreateForm.test.tsx
@@ -1,6 +1,17 @@
-import { screen } from "@testing-library/react";
-import { renderWithProviders } from "../../testUtils/renderWithProviders";
+import { act, fireEvent, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { mockEventCreate } from "../../mocks/mocks";
+import {
+  renderRouterWithProviders,
+  renderWithProviders,
+} from "../../testUtils/renderWithProviders";
 import CreateForm from "./CreateForm";
+
+const mockCreateEventFunction = jest.fn();
+
+jest.mock("../../hooks/useEvents/useEvents", () => () => ({
+  createEvent: mockCreateEventFunction,
+}));
 
 describe("Given a CreateForm componente", () => {
   describe("When it is rendered", () => {
@@ -23,6 +34,107 @@ describe("Given a CreateForm componente", () => {
       const expectedInput = screen.getByRole("textbox", { name: text });
 
       expect(expectedInput).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user writes in the 'Event name' input", () => {
+    test("Then it changes the value of this input", async () => {
+      const nameLabel = "Event name";
+      const nameText = "Sa Costa";
+
+      renderRouterWithProviders({}, <CreateForm />);
+
+      const nameInput = screen.getByLabelText(nameLabel);
+
+      await act(async () => await userEvent.type(nameInput, nameText));
+
+      expect(nameInput).toHaveValue(nameText);
+    });
+  });
+
+  describe("When the user submits the form", () => {
+    test("The crateEvent function should be called", async () => {
+      const eventNameInputPlaceHolderText = "Introduce an event name";
+      const distanceInputPlaceholderText = "Km";
+      const typeSelectPlaceHolderText = "Surface";
+      const dateInputPlaceHolderText = "Enter a valid date";
+      const descriptionTextAreaPlaceHolderText = "Add a description";
+      const imageInputPlaceHolderText = "Add an image";
+
+      const picture = new File(["event"], "event.jpg", {
+        type: "image/jpg",
+      });
+
+      renderRouterWithProviders({}, <CreateForm />);
+
+      const nameInputPlaceholder = screen.getByPlaceholderText(
+        eventNameInputPlaceHolderText
+      );
+
+      const distanceInputPlaceholder = screen.getByPlaceholderText(
+        distanceInputPlaceholderText
+      );
+
+      const typeInputPlaceholder = screen.getByPlaceholderText(
+        typeSelectPlaceHolderText
+      );
+
+      const dateInputPlaceholder = screen.getByPlaceholderText(
+        dateInputPlaceHolderText
+      );
+
+      const decriptionInputPlaceholder = screen.getByPlaceholderText(
+        descriptionTextAreaPlaceHolderText
+      );
+
+      const imageInputPlaceholder = screen.getByPlaceholderText(
+        imageInputPlaceHolderText
+      );
+
+      const submitButton = screen.getByRole("button");
+
+      await act(
+        async () =>
+          await userEvent.type(nameInputPlaceholder, mockEventCreate.name)
+      );
+
+      await act(
+        async () =>
+          await userEvent.type(
+            distanceInputPlaceholder,
+            mockEventCreate.distance
+          )
+      );
+
+      await act(
+        async () =>
+          await fireEvent.change(dateInputPlaceholder, {
+            target: { value: "12/02/23" },
+          })
+      );
+
+      await act(
+        async () =>
+          await userEvent.selectOptions(
+            typeInputPlaceholder,
+            mockEventCreate.type
+          )
+      );
+
+      await act(
+        async () =>
+          await userEvent.type(
+            decriptionInputPlaceholder,
+            mockEventCreate.description
+          )
+      );
+
+      await act(
+        async () => await userEvent.upload(imageInputPlaceholder, picture)
+      );
+      await act(async () => await userEvent.click(submitButton));
+
+      expect(mockCreateEventFunction).toHaveBeenCalled();
     });
   });
 });
