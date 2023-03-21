9e480fc2e42fea32324f01ee878c00ea868ba5a5
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-21 09:33:32 +0100

    Change testing because of adding lazy loading

--- src/Pages/NotFoundPage/NotFoundPage.test.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.test.tsx
@@ -1,37 +1,46 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import { renderRouterWithProviders } from "../../testUtils/renderWithProviders";
 import NotFoundPage from "./NotFoundPage";
 
 describe("Given a NotFoundPage component", () => {
   describe("When it is rendered", () => {
-    test("Then it should show an image with an alt text 'page-not-foun'", () => {
+    test("Then it should show an image with an alt text 'page-not-foun'", async () => {
       const expectedText = "page-not-found";
+      let expectedImage;
 
       renderRouterWithProviders({}, <NotFoundPage />);
 
-      const expectedImage = screen.getByTestId(expectedText);
+      await waitFor(() => (expectedImage = screen.getByTestId(expectedText)));
 
       expect(expectedImage).toBeInTheDocument();
     });
 
-    test("Then it should show a header with the text 'Oh NO!'", () => {
+    test("Then it should show a header with the text 'Oh NO!'", async () => {
       const headerText = "Oh NO!";
+      let expectedHeader;
 
       renderRouterWithProviders({}, <NotFoundPage />);
 
-      const expectedHeader = screen.getByRole("heading", { name: headerText });
+      await waitFor(
+        () =>
+          (expectedHeader = screen.getByRole("heading", { name: headerText }))
+      );
 
       expect(expectedHeader).toBeInTheDocument();
     });
 
-    test("Then it should show a button with the text 'Back to home page'", () => {
+    test("Then it should show a button with the text 'Back to home page'", async () => {
       const expectedText = "Back to home page";
+      let expectedButton;
 
       renderRouterWithProviders({}, <NotFoundPage />);
 
-      const expectedButton = screen.getByRole("button", {
-        name: expectedText,
-      });
+      await waitFor(
+        () =>
+          (expectedButton = screen.getByRole("button", {
+            name: expectedText,
+          }))
+      );
 
       expect(expectedButton).toBeInTheDocument();
     });
