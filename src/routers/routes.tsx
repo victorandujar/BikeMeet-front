248774d5f7a9feb0cc658fe0ba95a59f683e10d2
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-22 15:39:16 +0100

    Fix lazy loading and add testing for it at routes

--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -2,13 +2,15 @@ import App from "../components/App/App";
 import endpoints from "./types";
 import { createBrowserRouter, RouteObject } from "react-router-dom";
 import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
+import { lazy } from "react";
+import NotFoundPage from "../Pages/NotFoundPage/NotFoundPage";
 import LoginPage from "../Pages/LoginPage/LoginPage";
 import RegisterPage from "../Pages/RegisterPage/RegisterPage";
-import HomePage from "../Pages/HomePage/HomePage";
-import PrivatePage from "../Pages/PrivatePage/PrivatePage";
-import NotFoundPage from "../Pages/NotFoundPage/NotFoundPage";
-import CreatePage from "../Pages/CreatePage/CreatePage";
 import DetailPage from "../Pages/DetailPage/DetailPage";
+
+const HomePage = lazy(() => import("../Pages/HomePage/HomePage"));
+const PrivatePage = lazy(() => import("../Pages/PrivatePage/PrivatePage"));
+const CreatePage = lazy(() => import("../Pages/CreatePage/CreatePage"));
 
 const routes: RouteObject[] = [
   {
