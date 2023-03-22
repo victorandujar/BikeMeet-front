c3c1ae3cda367d117d939839e4382912ce115d56
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-03-22 19:25:21 +0100

    Modify options of select order

--- src/components/CreateForm/optionsSelect.ts
+++ src/components/CreateForm/optionsSelect.ts
@@ -1,5 +1,5 @@
 export const optionsSelect = [
-  { type: "Gravel", name: "Gravel", key: "1" },
-  { type: "Road", name: "Road", key: "2" },
-  { type: "Btt", name: "Btt", key: "3" },
+  { type: "Road", name: "Road", key: "1" },
+  { type: "Btt", name: "Btt", key: "2" },
+  { type: "Gravel", name: "Gravel", key: "3" },
 ];
